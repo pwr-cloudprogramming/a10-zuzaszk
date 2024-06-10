8abd77d32bfee01bae28bcb0a6c66e84ea733250
zuzaszk <zu.aszkielowicz@gmail.com>
2024-06-10 14:16:45 +0200

    add cors

--- backend/src/main/java/com/game/config/CorsConfig.java
+++ backend/src/main/java/com/game/config/CorsConfig.java
@@ -9,9 +9,12 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
